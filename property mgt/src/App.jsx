86bc1e387cb502e897d8a55c61f5aada12979f24
Chirchir4 <gloriachirchir4@gmail.com>
2023-01-30 13:27:58 +0300

    set up themes

--- property mgt/src/App.jsx
+++ property mgt/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import Topbar from './scenes/global/Topbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const[theme,colorMode]=useMode()
   return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<main className='content'>
+  <Topbar/>
+</main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
